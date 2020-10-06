f9bc6cac377594db50d188a71842a71300efb7a0
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-10-06 11:25:05 +0200

    add bg color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,18 @@
 <template>
   <v-app>
     <AppBar />
-    <v-main>
+    <v-main class="bg">
       <nuxt />
     </v-main>
     <Footer />
   </v-app>
 </template>
+
+<style scoped>
+.bg {
+  background-color: #eeeeee;
+}
+</style>
 
 <script>
 import AppBar from './../components/AppBar'
