e122063a11f10a462298611a1a571d7a166c8192
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-14 10:03:04 +0100

    add pages

--- pages/schede.vue
+++ pages/schede.vue
@@ -1,4 +1,46 @@
-<template></template>
+<template>
+  <v-row align="start" justify="center">
+    <v-col cols="12">
+      <v-banner elevation="10" rounded="lg" class="text-center gradient"
+        ><span class="white--text">SCHEDE ALLEVAMENTO</span></v-banner
+      >
+    </v-col>
+    <v-col cols="12">
+      <v-row
+        align="center"
+        justify="center"
+        v-for="scheda in schede"
+        :key="scheda.tipo"
+      >
+        <v-col cols="12" sm="12" md="12" lg="4" xl="4">
+          <v-img
+            alt="immagine"
+            :src="`/immagini/schede/${scheda.immagine}`"
+          ></v-img>
+        </v-col>
+        <v-col cols="12" sm="12" md="12" lg="8" xl="8">
+          <v-card rounded="lg" elevation="10">
+            <v-card-title>{{ scheda.tipo }}</v-card-title>
+            <v-card-text>
+              <p>
+                <span class="title">Terrario</span> <br />
+                <span class="body-2" v-html="scheda.terrario"></span>
+              </p>
+              <p>
+                <span class="title">Alimentazione</span> <br />
+                <span class="body-2" v-html="scheda.alimentazione"></span>
+              </p>
+              <p>
+                <span class="title">Riproduzione</span> <br />
+                <span class="body-2" v-html="scheda.riproduzione"></span>
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
 
 <script>
 export default {
@@ -6,6 +48,11 @@ export default {
     return {
       title: 'Schede tecniche',
     }
+  },
+
+  async asyncData({ $axios }) {
+    const schede = await $axios.$get('/data/schede.json')
+    return { schede }
   },
 }
 </script>
